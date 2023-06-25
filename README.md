d3af600770a7c9f47b77bd5f820b40302fc40f2a
wintersys <vortettyspam@gmail.com>
2023-06-25 00:57:23 -0700

    No warranty notice

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ Well why not, 'nuff said.
 
 <sub><sup><sub><sup>... Really tho know others have written ones before that look nice tbh i am just bored. Sue me lol</sup></sub></sup></sub>
 
+## Notice
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+<http://www.wtfpl.net/> for more details
+
 ## Building
 
 ### Prerequisites
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/* This program is free software. It comes without any warranty, to
+     * the extent permitted by applicable law. You can redistribute it
+     * and/or modify it under the terms of the Do What The Fuck You Want
+     * To Public License, Version 2, as published by Sam Hocevar. See
+     * http://www.wtfpl.net/ for more details. */
+
 // Games made using `agb` are no_std which means you don't have access to the standard
 // rust library. This is because the game boy advance doesn't really have an operating
 // system, so most of the content of the standard library doesn't apply.
